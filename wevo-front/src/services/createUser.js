987b71adab2b54633c82f70ffe9c4944d6bb4e20
DiogoB0mfim <diogohbomfim@gmail.com>
2022-12-04 13:19:12 -0300

    update: form de signup criado

---
+++ wevo-front/src/services/createUser.js
@@ -0,0 +1,26 @@
+import axios from "axios";
+import { BASE_URL } from "../constants/url";
+
+const createUser = (form, gender) => {
+  const body = {
+    name: form.name,
+    cpf: form.cpf,
+    email: form.email,
+    phone: form.phone,
+    gender: gender,
+    birth: form.birth,
+  };
+
+  axios
+    .post(`${BASE_URL}/create`, body)
+
+    .then((response) => {
+      alert("Usuário criado!");
+    })
+
+    .catch((error) => {
+      console.log(error.message);
+    });
+};
+
+export default createUser;
